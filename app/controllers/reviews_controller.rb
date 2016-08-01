49f685bb83aec457820bb4df85377f4eab3532a2
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-01 21:04:29 +0900

    add review edit function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,27 @@
 class ReviewsController < ApplicationController
 
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    review = Review.find(params[:id])
+    @tool_id = review.tool.id
+    if review.user_id.to_i == current_user.id
+      review.update(update_params)
+      redirect_to root_path
+    end
+  end
+
   def destroy
     review = Review.find(params[:id])
     review.destroy
     redirect_to controller: :users, action: :index
+  end
+
+  private
+  def update_params
+    params.require(:review).permit(:review).merge(user_id: current_user.id, tool_id: @tool_id)
   end
 
 end
