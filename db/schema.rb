84e14be56cbde689d6eae4c60c3110971853ebc9
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-03 17:41:38 +0900

    create Like model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729154720) do
+ActiveRecord::Schema.define(version: 20160803083759) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.text     "review",     limit: 65535
