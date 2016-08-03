97142da4b48631d3408c656f409e933c2c8a66ca
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-03 21:04:19 +0900

    add column likes_count to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803083759) do
+ActiveRecord::Schema.define(version: 20160803084418) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160803083759) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.integer  "likes_count",            limit: 4
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
