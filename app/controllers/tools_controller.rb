f0f772c0598a9de8ff05929044f9b5acfff889c1
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-04 15:37:58 +0900

    set authenticate_user!

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,4 +1,5 @@
 class ToolsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_tool, only: [:show, :edit, :update, :destroy]
 
   autocomplete :tool, :name, :full => true
