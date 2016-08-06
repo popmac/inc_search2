6a402f013499f8c9536b4f4541209a15f1b4797a
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-06 19:54:18 +0900

    association user, review, comment

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :tool
   has_many :likes, dependent: :destroy
+  has_many :comments
   validates :review, presence: true
   def like_user(user_id)
    likes.find_by(user_id: user_id)
