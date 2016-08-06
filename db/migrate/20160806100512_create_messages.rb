3e09a34ef2bed096e343c5ec767096f23ff27f3b
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-06 19:20:44 +0900

    delete message model

--- db/migrate/20160806100512_create_messages.rb
+++
@@ -1,9 +0,0 @@
-class CreateMessages < ActiveRecord::Migration
-  def change
-    create_table :messages do |t|
-      t.text :message
-      t.integer :user_id
-      t.timestamps
-    end
-  end
-end
