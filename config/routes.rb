6c955f57f2d7e414e801ef33bc005c5eee3fdf05
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-06 18:50:23 +0900

    routing messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users do
+    resources :messages
+  end
   resources :tools do
     collection do
       get 'search'
