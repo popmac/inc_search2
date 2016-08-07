e302b84fde681b0b12cf3d074687334b30dd460f
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-08-07 16:01:29 +0900

    delete comments when delete review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :tool
   has_many :likes, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :review, presence: true
   def like_user(user_id)
    likes.find_by(user_id: user_id)
